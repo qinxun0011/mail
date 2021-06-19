b91fbc4e711bc409dfd91e818879f388795d7796
tree <56065228+qinxun0011@users.noreply.github.com>
2021-06-19 13:33:06 +0800

    模块的划分

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>
+  </div>
 </template>
 
 <script>
+import MainTabBar from "@/components/content/mainTabBar/MainTabBar";
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    MainTabBar
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import VueRouter from "@/router";
 
-createApp(App).mount('#app')
+createApp(App).use(VueRouter).mount('#app')

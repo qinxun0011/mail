f2e898b2cb4e78b232ba21794fdb9759f9bfb11c
tree <56065228+qinxun0011@users.noreply.github.com>
2021-06-19 11:35:31 +0800

    style文件引入

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            extensions: [],
+            //配置别名
+            alias: {
+                //默认配置了
+                //'@': 'src'
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
+    }
+}
